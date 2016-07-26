78065a6a318a330b64facbede4352e9256a958ea
1000ch <shogo.sensui@gmail.com>
2016-07-27 02:54:09 +0900

    Remove unused gulpfile.js

--- test/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var DependencyVisualizer = require('../');
-
-gulp.task('visualizer', function () {
-  gulp.src('./fixtures/*.js')
-    .pipe(DependencyVisualizer());
-});
-
-gulp.task('default', ['visualizer']);
